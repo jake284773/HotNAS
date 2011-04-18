aed0e6f2311ee2d9b580bc04ed853ed19876ed30
Jake Moreman <oakenhaze@gmail.com>
2011-04-18 14:13:58 +0100

    Modified codeigniter database config

--- package/customize/source/frontend/www/application/config/database.php
+++ package/customize/source/frontend/www/application/config/database.php
@@ -41,12 +41,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '';
 $db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['database'] = 'sqlite' . APPPATH . 'db/database.sqlite';
+$db['default']['dbdriver'] = "pdo_sqlite";
+$db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
